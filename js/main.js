e883724aaa9787535452f57aadfc0968e0aad448
Raad-Hanandeh <138323419+Raad-Hanandeh@users.noreply.github.com>
2024-01-27 22:05:59 +0300

    last

--- js/main.js
+++ js/main.js
@@ -24,7 +24,90 @@ let imgFour = document.getElementById("imgFour")
 
 let numBag = document.getElementById("numBag")
 
-imgOne.onclick = ()=>{
+
+imgOne.addEventListener("click" , add)
+imgTwo.addEventListener("click" , add)
+imgThree.addEventListener("click" , add)
+imgFour.addEventListener("click" , add)
+
+
+// function add(){
+//     let value = Number(numBag.innerText) + 1
+//     numBag.innerHTML=value
+// scrollTo({
+//     left:0,
+//     top:0,
+//     behavior:"smooth"
+
+// })
+// }
+
+// imgOne.onclick = ()=>{
+//     let value = Number(numBag.innerText) + 1
+//     numBag.innerHTML=value
+// scrollTo({
+//     left:0,
+//     top:0,
+//     behavior:"smooth"
+
+// })
+
+// }
+// imgTwo.onclick = ()=>{
+//     let value = Number(numBag.innerText) + 1
+//     numBag.innerHTML=value
+// scrollTo({
+//     left:0,
+//     top:0,
+//     behavior:"smooth"
+
+// })
+
+// }
+// imgThree.onclick = ()=>{
+//     let value = Number(numBag.innerText) + 1
+//     numBag.innerHTML=value
+// scrollTo({
+//     left:0,
+//     top:0,
+//     behavior:"smooth"
+
+// })
+
+// }
+
+// imgFour.onclick = ()=>{
+//     let value = Number(numBag.innerText) + 1
+//     numBag.innerHTML=value
+// scrollTo({
+//     left:0,
+//     top:0,
+//     behavior:"smooth"
+
+// })
+
+// }
+
+
+// ************************
+
+let item1 = document.getElementById("itemOne")
+let item2 = document.getElementById("itemTwo")
+let item3 = document.getElementById("itemThree")
+let item4 = document.getElementById("itemFour")
+let item5 = document.getElementById("itemFive")
+let item6 = document.getElementById("itemSix")
+
+item1.addEventListener("click", add)
+item2.addEventListener("click", add)
+item3.addEventListener("click", add)
+item4.addEventListener("click", add)
+item5.addEventListener("click", add)
+item6.addEventListener("click", add)
+
+
+
+function add(){
     let value = Number(numBag.innerText) + 1
     numBag.innerHTML=value
 scrollTo({
@@ -33,43 +116,6 @@ scrollTo({
     behavior:"smooth"
 
 })
-
-}
-imgTwo.onclick = ()=>{
-    let value = Number(numBag.innerText) + 1
-    numBag.innerHTML=value
-scrollTo({
-    left:0,
-    top:0,
-    behavior:"smooth"
-
-})
-
-}
-imgThree.onclick = ()=>{
-    let value = Number(numBag.innerText) + 1
-    numBag.innerHTML=value
-scrollTo({
-    left:0,
-    top:0,
-    behavior:"smooth"
-
-})
-
 }
 
-imgFour.onclick = ()=>{
-    let value = Number(numBag.innerText) + 1
-    numBag.innerHTML=value
-scrollTo({
-    left:0,
-    top:0,
-    behavior:"smooth"
-
-})
-
-}
-
-
-// let value = Number(totalElement.innerText) + Number(price)
-// totalElement.innerHTML = value
+console.log(item2)
