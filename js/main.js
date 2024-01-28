ac806198d3aaa104ac2682dbb1f9e6c996672770
tawfeekamr <tawfeekamr91@gamil.com>
2024-01-28 23:00:51 +0300

    Add new code for cart

--- js/main.js
+++ js/main.js
@@ -2,6 +2,50 @@ let btnMenu = document.getElementById("menu")
 let btnClose = document.getElementById("close")
 let container = document.querySelector(".container")
 
+const productsData = [
+    {
+        id: 1,
+        url: "./image/img-enjoy1.png",
+        name: "NATIVE IRON CHAIR",
+        price: 4990
+    },
+    {
+        id: 2,
+        url: "./image/Rectangleenjoy2.png",
+        name: "CENTURY DASHE",
+        price: 4990
+    },
+    {
+        id: 3,
+        url: "./image/Rectangle 545.png",
+        name: "NATIVE LIGHT CHAIR",
+        price: 4990
+    },
+    {
+        id: 4,
+        url: "./image/Rectangle 545.png",
+        name: "PAOLA WOOD LAMP",
+        price: 5990
+    }
+]
+
+const imageMaster = document.querySelector(".img-product");
+
+productsData.forEach(product => {
+    imageMaster.innerHTML += `
+                    <div>
+                     <div class="item-cart">
+                         <img alt="" class="img-hover" id="imgOne" src="${product.url}">
+                        <button class="cart-btn" onclick="addToCart(${product.id})">Add to cart</button>
+                    </div>
+                    <div class="text-pro">
+                        <p>${product.name}</p>
+                        <span>$${product.price}</span>
+                    </div>
+</div>
+    
+    `
+})
 
 container.classList.add("hide")
 
@@ -25,10 +69,10 @@ let imgFour = document.getElementById("imgFour")
 let numBag = document.getElementById("numBag")
 
 
-imgOne.addEventListener("click" , add)
-imgTwo.addEventListener("click" , add)
-imgThree.addEventListener("click" , add)
-imgFour.addEventListener("click" , add)
+// imgOne.addEventListener("click" , add)
+// imgTwo.addEventListener("click" , add)
+// imgThree.addEventListener("click" , add)
+// imgFour.addEventListener("click" , add)
 
 
 // function add(){
@@ -90,32 +134,37 @@ imgFour.addEventListener("click" , add)
 
 
 // ************************
-
-let item1 = document.getElementById("itemOne")
-let item2 = document.getElementById("itemTwo")
-let item3 = document.getElementById("itemThree")
-let item4 = document.getElementById("itemFour")
-let item5 = document.getElementById("itemFive")
-let item6 = document.getElementById("itemSix")
-
-item1.addEventListener("click", add)
-item2.addEventListener("click", add)
-item3.addEventListener("click", add)
-item4.addEventListener("click", add)
-item5.addEventListener("click", add)
-item6.addEventListener("click", add)
-
-
-
+//
+// let item1 = document.getElementById("itemOne")
+// let item2 = document.getElementById("itemTwo")
+// let item3 = document.getElementById("itemThree")
+// let item4 = document.getElementById("itemFour")
+// let item5 = document.getElementById("itemFive")
+// let item6 = document.getElementById("itemSix")
+//
+// item1.addEventListener("click", add)
+// item2.addEventListener("click", add)
+// item3.addEventListener("click", add)
+// item4.addEventListener("click", add)
+// item5.addEventListener("click", add)
+// item6.addEventListener("click", add)
+//
+//
+//
 function add(){
     let value = Number(numBag.innerText) + 1
     numBag.innerHTML=value
-scrollTo({
-    left:0,
-    top:0,
-    behavior:"smooth"
+        scrollTo({
+            left:0,
+            top:0,
+            behavior:"smooth"
 
-})
+        })
 }
+//
 
-console.log(item2)
+
+function addToCart(productId) {
+    const data = productsData.filter(item => item.id === productId)[0];
+    localStorage.setItem("cart", JSON.stringify(data))
+}
